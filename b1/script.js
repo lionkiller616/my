16909fc28299918336cecfbcbe57ea1494ae6c3c
Rang lal <ranglal616@gmail.com>
2023-07-18 14:11:49 -0700

    start

---
+++ b1/script.js
@@ -0,0 +1,11 @@
+alert("welcome")
+$(document).ready(function(){
+  $('.title').click(function(){
+    $('.container').addClass('open');
+  });
+  
+  
+  $('.close').click(function(){
+    $('.container').removeClass('open');
+  });
+});
